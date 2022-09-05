2cc73bad670729ebf7d3d37b040bb2ede8dd8a8c
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-09-05 14:17:47 -0400

    -added import render_template -added error method

--- To_Do_webapp/flaskr/helpers.py
+++ To_Do_webapp/flaskr/helpers.py
@@ -1,4 +1,4 @@
-from flask import g, request, redirect, url_for
+from flask import g, request, redirect, url_for, render_template
 
 def login_required(f):   
     def decorated_function(*args, **kwargs):
@@ -6,3 +6,6 @@ def login_required(f):
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
+
+def error(message, code=400):
+    return render_template("error.html", top=code,bottom=message)
