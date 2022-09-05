e662982fc845c0b927df17c80450a8b61c3cd107
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-09-05 11:02:22 -0400

    first commit

--- To_Do_webapp/flaskr/main.py
+++ To_Do_webapp/flaskr/main.py
@@ -1,32 +1,54 @@
 import os
+from pickle import FALSE
 
-from flask import Flask
+from flask import Flask,session,redirect,request,render_template
+from flask_session import Session
+from flask_mysqldb import MySQL
+
+from helpers import login_required
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# create and configure the app
+app = Flask(__name__)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# auto reload templates
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# config sessions
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+Session(app)
 
-    return app
+# config database
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config["MYSQL_PASSWORD"] = 'Password'
+app.config['MYSQL_DB'] = 'to_do_webapp'
+
+mysql = MySQL(app)
+print("mysql object", mysql.connection)
+# create a cursor
+
+
+#cursor.execute(''' IF NOT EXISTS (SELECT * FROM sys.tables WHERE NAME = 'ToDo) ''')
+
+
+
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+# a simple page that says hello
+@app.route('/')
+def home_page():    
+    return render_template("layout.html")
+
+@app.route('/register')
+def register():
+    return 'not yet done'
+
+
+if __name__ == '__main__':
+    app.run()
